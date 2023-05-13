30f2cd8cb55fca9a9b38064747c870007755652d
D-coding-GH <daniel_hitchen@outlook.com>
2023-05-13 15:09:18 +0100

    Add files via upload

---
+++ src/Movies/CreateMovie.tsx
@@ -0,0 +1,12 @@
+
+export default function CreateMovie() {
+
+    return (
+        <>
+            <h3>Create Movie</h3>
+           
+        </>
+
+    )
+
+}
---
+++ src/Movies/EditMovie.tsx
@@ -0,0 +1,11 @@
+export default function EditMovie() {
+
+    return (
+        <>
+            <h3>Edit Movie</h3>
+
+        </>
+
+    )
+
+}
